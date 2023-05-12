8a023e6f138ed7ba2608998cd49b1852302f72dc
rtree <arakitomohiko@gmail.com>
2023-05-13 00:20:44 +0100

    prepare react based project

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { goerli, polygonMumbai, scrollTestnet, optimism } from 'wagmi/chains'
+import { Web3Button } from '@web3modal/react'
+import { useAccount, useContract } from 'wagmi'
+
 import './App.css';
 
+function HomePage() {
+  return <Web3Button />
+}
 function App() {
+  const chains = [goerli, polygonMumbai, scrollTestnet, optimism]
+  const projectId = '5b90bbc69d8aceaccdf7dab68ee6ae91'
+  const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors: w3mConnectors({ projectId, version: 1, chains }),
+    publicClient
+  })
+  const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <WagmiConfig config={wagmiConfig}>
+        <HomePage />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
   );
 }
 
